c5949d68fd349c73c55cc4d91fe69399800cd3d0
Kayfari <mr.osaeafari@gmail.com>
2024-09-03 19:15:35 +0000

    added banners to banner section

--- services/envService.ts
+++ services/envService.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 const envs = {
-    api_url: z.string().parse(process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080')
+    // api_url: z.string().parse(process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080')
+    api_url: z.string().parse(process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080/api')
+    //api_url: z.string().parse(process.env.EXPO_PUBLIC_API_URL ?? 'campaignwithus.com/public/api')
+
 };
 
 export default envs;
