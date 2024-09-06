1fb1d8ba571683391da5c089fa9d660b33d714ce
linuxdev <asieduernest@yahoo.co.uk>
2024-09-06 04:06:30 -0400

    Update/ui: nativewinding

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,22 @@
+// module.exports = function (api) {
+// 	api.cache(true);
+// 	return {
+// 		presets: [
+// 			[
+// 				"babel-preset-expo",
+// 				{
+// 					jsxImportSource: "nativewind"
+// 				}
+// 			],
+// 			"nativewind/babel"
+// 		],
+// 		plugins: ["react-native-reanimated/plugin"]
+// 	};
+// };
+
 module.exports = function (api) {
 	api.cache(true);
 	return {
-		presets: [
-			[
-				"babel-preset-expo",
-				{
-					jsxImportSource: "nativewind"
-				}
-			],
-			"nativewind/babel"
-		],
-		plugins: ["react-native-reanimated/plugin"]
+		presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"]
 	};
 };
