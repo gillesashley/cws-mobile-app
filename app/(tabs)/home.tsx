d2eb0ae3808f83ae69031f3ff9314a16b2f04dea
linuxdev <asieduernest@yahoo.co.uk>
2024-09-27 04:36:52 -0400

    update/ui: refactor banner rendering and interval

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FlatList, Image, Modal, RefreshControl, ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -14,7 +14,7 @@ import { CampaignMessage, useApi } from "@/services/services";
 import mtn_banner from "@/assets/images/mtn_banner.jpeg";
 import guinness_banner from "@/assets/images/guinness_banner.jpeg";
 import useSWR, { useSWRConfig } from "swr";
-
+import { useIterator } from "../../hooks/useIterator";
 interface CampaignSectionProps {
     title: string;
     description: string;
@@ -35,10 +35,10 @@ const CampaignSection: React.FC<CampaignSectionProps> = ({ title, description, c
             horizontal
             showsHorizontalScrollIndicator={false}
             data={campaigns?.slice(0, 3)}
-            renderItem={({ item }) => <CampaignPost {...item} />}
+            renderItem={({ item }) => <View className="nfc-flatlist-item mx-2 w-[49vw]"><CampaignPost  {...item} /></View> }
             keyExtractor={(item) => item.id}
             contentContainerStyle={styles.campaignScroll}
-            className="nfc-campaignSection:flatlist"
+            className="nfc-campaignSection:flatlist w-[100vw] flex gap-5 "
         />
     </View>
 );
@@ -52,7 +52,12 @@ export default function HomeScreen() {
     const { data: nationalCampaigns, isLoading: qryNationalCampaignsLoading } = api.getCampaignsNational();
     const { data: constituencyCampaigns, isLoading: qryCampaignLoading, error: qryCampaignError, mutate: refreshCampaigns } = api.getCampaignsConstituency();
     const { data: userBalance, isLoading: qryBalanceLoading, error: qryBalanceError, mutate: refreshBalance } = api.getUserBalance();
-    const [bannerIdx, setBannerIdx] = useState<undefined|number>();
+
+    const nextCtyBanner = useIterator(ctyBanners)
+    const nextRgBanner = useIterator(rgBanners)
+    const nextNtBanner = useIterator(ntBanners)
+    const nextDefaultBanner = useIterator([guinness_banner,mtn_banner],6_000)
+
     const {mutate}= useSWRConfig()
 
     const isLoading = qryBalanceLoading || qryCampaignLoading || qryNationalCampaignsLoading || qryRegionalCampaignsLoading||qryBannersLoading||qryRgBannersLoading||qryNtBannersLoading;
@@ -65,13 +70,6 @@ export default function HomeScreen() {
 
     const onRefresh = () => Promise.allSettled([refreshBalance(), refreshCampaigns(),mutate(()=>true)]);
 
-    useEffect(() => {
-        if (!ctyBanners?.length) {
-            return;
-        }
-
-        setInterval(() => setBannerIdx((prevIdx) => ((prevIdx ?? 0) + 1)%ctyBanners.length),1_000*2);
-    }, []);
 
     if (isLoading) {
         return (
@@ -84,9 +82,11 @@ export default function HomeScreen() {
     return (
         <SafeAreaView style={[styles.container, { backgroundColor }]} edges={["top"]}>
             <Header balance={userBalance?.balance} onBalancePress={() => router.push("/points-payment")} />
+            
             <ScrollView refreshControl={<RefreshControl refreshing={isLoading} onRefresh={onRefresh} colors={["#9Bd35A", "#689F38"]} />}>
-                <View style={styles.bannerContainer}>
-                    <Image source={(ctyBanners?.length &&ctyBanners?.at(bannerIdx).image_url)||mtn_banner} style={styles.banner} resizeMode="cover" />
+                
+                <View style={styles.bannerContainer} className="mt-5">
+                    <Image source={nextCtyBanner?.image_url ||nextDefaultBanner} style={styles.banner} resizeMode="cover" />
                 </View>
 
                 <CampaignSection
@@ -97,7 +97,7 @@ export default function HomeScreen() {
                 />
 
                 <View style={styles.bannerContainer}>
-                    <Image source={guinness_banner} style={styles.extraBanner} resizeMode="cover" />
+                    <Image source={nextRgBanner?.image_url||nextDefaultBanner} style={styles.extraBanner} resizeMode="cover" />
                 </View>
 
                 <CampaignSection
@@ -114,9 +114,11 @@ export default function HomeScreen() {
                     onSeeAll={() => setShowAllCampaigns(true)}
                 />
             </ScrollView>
+
             <Modal visible={showAllCampaigns} animationType="slide" onRequestClose={() => setShowAllCampaigns(false)}>
                 <CampaignList campaignMessages={constituencyCampaigns} onClose={() => setShowAllCampaigns(false)} />
             </Modal>
+            
             <ErrorView error={JSON.stringify(error?.message ?? error, null, 2)} onRetry={() => onRefresh()} />
         </SafeAreaView>
     );
